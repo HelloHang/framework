d6519d0ac8771e55be6b4961a66e4b0877c3e19b
HelloHang <544533631@qq.com>
2018-07-07 18:03:12 +0800

    Update DatabaseHelper

--- chapter1/src/main/java/com/daniels/helper/DatabaseHelper.java
+++ chapter1/src/main/java/com/daniels/helper/DatabaseHelper.java
@@ -1,12 +1,17 @@
 package com.daniels.helper;
 
+import com.daniels.util.CollectionUtil;
 import com.daniels.util.PropertiesUtil;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -50,18 +55,111 @@ public class DatabaseHelper {
   }
 
   public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params) {
-    List entities;
+    List<T> entities;
     Connection connection = getConnection();
     try {
       entities = QUERY_RUNNER
-          .execute(connection, sql, new BeanHandler<T>(entityClass), params);
+          .query(connection, sql, new BeanListHandler<T>(entityClass), params);
     } catch (SQLException e) {
       LOGGER.error("Query entity list failed.", e);
       throw new RuntimeException(e);
-    }
-    finally {
+    } finally {
       closeConnection(connection);
     }
     return entities;
+  }
+
+  public static <T> T queryEntity(Class<T> entityClass, String sql, Object... params) {
+    T entity;
+    Connection connection = getConnection();
+    try {
+      entity = QUERY_RUNNER
+          .query(connection, sql, new BeanHandler<T>(entityClass), params);
+    } catch (SQLException e) {
+      LOGGER.error("Query entity list failed.", e);
+      throw new RuntimeException(e);
+    } finally {
+      closeConnection(connection);
+    }
+    return entity;
+  }
+
+  public static List<Map<String, Object>> executeQuery(String sql, Object... params) {
+    List<Map<String, Object>> result;
+    Connection connection = getConnection();
+    try {
+      result = QUERY_RUNNER
+          .query(connection, sql, new MapListHandler(), params);
+    } catch (SQLException e) {
+      LOGGER.error("Query entity list failed.", e);
+      throw new RuntimeException(e);
+    } finally {
+      closeConnection(connection);
+    }
+    return result;
+  }
+
+  public static int executeUpdate(String sql, Object... params) {
+    int row = 0;
+    Connection connection = getConnection();
+    try {
+      row = QUERY_RUNNER.update(connection, sql, params);
+    } catch (SQLException e) {
+      LOGGER.error("Execute update failed.", e);
+      throw new RuntimeException(e);
+    } finally {
+      closeConnection(connection);
+    }
+    return row;
+  }
+
+  public static <T> boolean insertEntity(Class<T> entityClass, Map<String, Object> fields) {
+    if (CollectionUtil.isEmpty(fields)) {
+      LOGGER.error("Can't insert entity: fields is empty.");
+      return false;
+    }
+
+    String sql = "INSERT INTO " + getTableName(entityClass);
+    StringBuilder columns = new StringBuilder("(");
+    StringBuilder values = new StringBuilder("(");
+    for (String fieldName : fields.keySet()) {
+      columns.append(fieldName).append(", ");
+      values.append("?, ");
+    }
+    columns.replace(columns.lastIndexOf(", "), columns.length(), ")");
+    values.replace(columns.lastIndexOf(", "), values.length(), ")");
+
+    sql += columns + " VALUES " + values;
+
+    Object[] params = fields.values().toArray();
+    return executeUpdate(sql, params) == 1;
+  }
+
+  public static <T> boolean updateEntity(Class<T> entityClass, long id,
+      Map<String, Object> fields) {
+    if (CollectionUtil.isEmpty(fields)) {
+      LOGGER.error("Update entity failed: fields is empty.");
+      return false;
+    }
+
+    StringBuilder sql = new StringBuilder("UPDATE ").append(getTableName(entityClass))
+        .append(" SET ");
+    StringBuilder conlumns = new StringBuilder();
+    for (String fieldName : fields.keySet()) {
+      conlumns.append(fieldName).append("=?, ");
+    }
+    sql.append(conlumns.substring(0, conlumns.lastIndexOf(", "))).append(" WHERE id=?");
+    List<Object> params = new ArrayList<Object>(fields.values());
+    params.add(id);
+    return executeUpdate(sql.toString(), params) == 1;
+  }
+
+  public static <T> boolean deleteEntity(Class<T> entityClass, long id) {
+    String sql = "DELETE FROM " + getTableName(entityClass) + " WHERE id=?";
+    return executeUpdate(sql, id) == 1;
+  }
+
+  private static String getTableName(Class<?> entityClass) {
+    return entityClass.getSimpleName();
   }
 }
--- chapter1/src/main/java/com/daniels/service/CustomerService.java
+++ chapter1/src/main/java/com/daniels/service/CustomerService.java
@@ -2,9 +2,7 @@ package com.daniels.service;
 
 import com.daniels.helper.DatabaseHelper;
 import com.daniels.model.Customer;
-import com.daniels.util.PropertiesUtil;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -23,29 +21,8 @@ public class CustomerService {
   private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
   public List<Customer> getCustomers() {
-    Connection connection = null;
-    List<Customer> customers = new ArrayList<Customer>();
     String sql = "SELECT * FROM customer";
-    try {
-      connection = DatabaseHelper.getConnection();
-      PreparedStatement statement = connection.prepareStatement(sql);
-      ResultSet resultSet = statement.executeQuery();
-      while (resultSet.next()) {
-        Customer customer = new Customer();
-        customer.setId(resultSet.getLong("id"));
-        customer.setName(resultSet.getString("name"));
-        customer.setContact(resultSet.getString("contact"));
-        customer.setEmail(resultSet.getString("email"));
-        customer.setRemark(resultSet.getString("remark"));
-        customer.setTelephone(resultSet.getString("telephone"));
-        customers.add(customer);
-      }
-    } catch (SQLException e) {
-      LOGGER.error("execute sql failed.", e);
-    } finally {
-      DatabaseHelper.closeConnection(connection);
-    }
-    return customers;
+    return DatabaseHelper.queryEntityList(Customer.class, sql);
   }
 
   public List<Customer> getCustomers(String keyword) {
